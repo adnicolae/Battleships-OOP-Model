d122f2399d10c1d8a7c3d95409d7bbb3a974e7a7
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-08-17 21:44:41 +0300

    Add project files.

---
+++ BattleshipsModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleshipsModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
