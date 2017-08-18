06cea93b5a3d3e9e0b42f18640522a7b46b31ef4
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-08-18 12:04:22 +0300

    Added exception handling

--- BattleshipsModel/Game.cs
+++ BattleshipsModel/Game.cs
@@ -7,6 +7,23 @@ namespace BattleshipsModel
         static void Main(string[] args)
         {
             Board board = new Board(10);
+            try
+            {
+                SquareLocation squareLocation = new SquareLocation(5, 20, board);
+                //Console.WriteLine(board.OnBoard(squareLocation));
+            }
+            catch(NotOnBoardException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(BattleshipException)
+            {
+                Console.WriteLine("Unhandled BattleshipException");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Unhandled Exception: " + ex);
+            }
         }
     }
 }
